6ea526b1733acb4dead9c64e89e95118c7a5a12f
Bragon76 <pierre.leplay@gmail.com>
2024-01-31 12:05:04 +0100

    Correction sur les unammed classes

--- src/test/java/fr/softeam/java21/G_Unnamed_Classes_Et_Instance_Main_method/HelloWorld.java
+++ src/test/java/fr/softeam/java21/G_Unnamed_Classes_Et_Instance_Main_method/HelloWorld.java
@@ -1,7 +1,7 @@
 package fr.softeam.java21.G_Unnamed_Classes_Et_Instance_Main_method;
 
 public class HelloWorld {
-    public static void main() {
+    public static void main(String[] arg) {
         System.out.println("Hello World!");
     }
 }
